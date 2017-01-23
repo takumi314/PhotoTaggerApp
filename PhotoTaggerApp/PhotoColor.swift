e83bc4caa1f2e264740304f75ef7d0cf6636e81a
takumi314 <isospin_symmetry@me.com>
2017-01-24 01:57:16 +0900

    make a struct about PhotoColor

---
+++ PhotoTaggerApp/PhotoColor.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoColor.swift
+//  PhotoTaggerApp
+//
+//  Created by NishiokaKohei on 2017/01/24.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoColor {
+    var red: Int?
+    var green: Int?
+    var blue: Int?
+    var colorName: String?
+}
