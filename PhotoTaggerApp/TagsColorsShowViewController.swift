89682fe7f3c750bbdf7c5b56af411add56224a28
takumi314 <isospin_symmetry@me.com>
2017-01-27 02:20:44 +0900

    create two ViewControllers in ViewControllers

---
+++ PhotoTaggerApp/TagsColorsShowViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TagsColorsShowViewController.swift
+//  PhotoTaggerApp
+//
+//  Created by NishiokaKohei on 2017/01/27.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
