adda49116f4d43d83082e49716f2620cd1f8b741
takumi314 <isospin_symmetry@me.com>
2017-01-29 01:00:07 +0900

    add UITableViewDataSource

--- PhotoTaggerApp/ListTableViewController.swift
+++ PhotoTaggerApp/ListTableViewController.swift
@@ -7,3 +7,34 @@
 //
 
 import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    // MARK: - IBOulets
+
+    // MARK: - Properties
+    var data = [AnyObject]()
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+extension ListTableViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cellData = data[indexPath.row]
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "TagOrColorCell", for: indexPath)
+//        cell.textLabel?.text = cellData.label
+//        return cell
+//    }
+
+}
