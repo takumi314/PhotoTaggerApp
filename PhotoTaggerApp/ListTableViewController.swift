57b3e9ab273b2ea11d7c350e4047ff2d2a54dc0d
takumi314 <isospin_symmetry@me.com>
2017-01-29 01:08:40 +0900

    make a extension inherited UITableViewDelegate

--- PhotoTaggerApp/ListTableViewController.swift
+++ PhotoTaggerApp/ListTableViewController.swift
@@ -38,3 +38,27 @@ extension ListTableViewController {
     }
 
 }
+
+// MARK: - UITableViewDelegate
+extension ListTableViewController {
+
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let cellData = data[indexPath.row]
+        guard let
+            color = cellData.color else {
+            cell.textLabel?.textColor = .black
+            cell.backgroundColor = .white
+            return
+        }
+
+        var red = CGFloat(0.0), green = CGFloat(0.0), blue = CGFloat(0.0), alpha = CGFloat(0.0)
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        let threshold = CGFloat(105)
+        let bgDelta = ((red * 0.299) + (green * 0.587) + (blue * 0.114));
+
+        let textColor: UIColor = (255 - bgDelta < threshold) ? .black : .white;
+        cell.textLabel?.textColor = textColor
+        cell.backgroundColor = color
+    }
+
+}
