30e7ad5152774f53472020d7280ded56ad3674ea
takumi314 <isospin_symmetry@me.com>
2017-01-29 00:55:41 +0900

    make TagsColorTableData class

---
+++ PhotoTaggerApp/TagsColorTableData.swift
@@ -0,0 +1,14 @@
+//
+//  TagsColorTableData.swift
+//  PhotoTaggerApp
+//
+//  Created by NishiokaKohei on 2017/01/29.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+struct TagsColorTableData {
+    var label: String
+    var color: UIColor?
+}
